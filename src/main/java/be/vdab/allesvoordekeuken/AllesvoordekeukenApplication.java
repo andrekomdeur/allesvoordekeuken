72bf2f3e1435b0971beaa2f524a92210950366a7
andre <andre.komdeur@gmail.com>
2020-04-20 11:58:58 +0200

    Initial commit

---
+++ src/main/java/be/vdab/allesvoordekeuken/AllesvoordekeukenApplication.java
@@ -0,0 +1,9 @@
+package be.vdab.allesvoordekeuken;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AllesvoordekeukenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AllesvoordekeukenApplication.class, args);
+    }
+}
